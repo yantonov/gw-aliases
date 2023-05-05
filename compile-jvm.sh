8655f16e3557175fd50aa539f66193402b3d1613
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2023-05-05 09:32:04 +0200

    add information messages about compiling steps

--- compile-jvm.sh
+++ compile-jvm.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset
 
+echo "Check compile java task"
 COMPILE_JAVA=$(gw tasks --all | grep 'compileJava' | head -n 1 || "")
+echo "Check compile scala task"
 COMPILE_SCALA=$(gw tasks --all | grep 'compileScala' | head -n 1 || "")
 
 if [ -n "${COMPILE_JAVA}" ]; then
+    echo "Compile java ..."
     gw compileJava compileTestJava
 fi
 
 if [ -n "${COMPILE_SCALA}" ]; then
+    echo "Compile scala ..."
     gw compileScala compileTestScala
 fi
 
